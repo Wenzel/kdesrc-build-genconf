78f9916efbaff273f5352e1c3d4f935f0b8ba292
Mathieu Tarral <mathieu.tarral@gmail.com>
2016-01-02 21:10:54 +0100

    able to display the tree of declared elements inside a config file

--- Config.py
+++ Config.py
@@ -4,46 +4,25 @@ import logging
 
 from PyQt5.QtGui import QStandardItemModel,QStandardItem
 
-class Module():
+class Include():
 
-    def __init__(self):
-        self.name = ''
-        self.repository = ''
-        self.cmake_opts = ''
-        self.branch = ''
+    def __init__(self, filepath):
+        self.filepath = filepath
 
-    def setName(self, name):
-        self.name = name
+class Section():
 
-    def setRepository(self, repository):
-        self.repository = repository
-
-    def setCmakeOpts(self, cmake_opts):
-        self.cmake_opts = cmake_opts
-
-    def setBranch(self, branch):
-        self.branch = branch
-
-
-class ModuleSet(Module):
-
-    def __init__(self):
-        super().__init__()
-        self.use_modules = []
-
-    def setUseModules(self, use_modules):
-        self.use_modules = use_modules
-
+    def __init__(self, type, content, id=None,):
+        self.type = type
+        self.id = id
+        self.content = content
 
 class ConfigFile():
 
     def __init__(self, filepath):
         self.filepath = filepath
         self.filename = os.path.basename(self.filepath)
-        self.fd = open(self.filepath, 'r')
-        self.elements = []
+        self.declarations = []
         self.includes = []
-
 
     def getNextLine(self):
         # skip comments
@@ -67,120 +46,49 @@ class ConfigFile():
         return next_line
 
     def load(self):
+        self.fd = open(self.filepath, 'r')
         logging.debug('loading new file <{}>'.format(self.filepath))
         next_line = self.getNextLine()
-        logging.debug('<{}>'.format(next_line))
+        grammar_sections = ['global', 'module', 'module-set', 'options']
+        grammar_key_value = ['include']
         while(next_line):
-            # module-set declaration ?
-            match = re.match(r'^\s*module-set(?:\s+(?P<module_name>.*))?\s*$', next_line)
-            if match:
-                self.loadModuleSet(match.group(1))
-            # module declaration ?
-            match = re.match(r'^\s*module(?:\s+(?P<module_name>.*))?\s*$', next_line)
-            if match:
-                self.loadModule(match.group(1))
-            # include ?
-            match = re.match(r'^\s*include\s+(?P<filepath>.*)\s*$', next_line)
-            if match:
-                include_path = os.path.expanduser(match.group(1))
-                # relative ?
-                if include_path[0] != '/':
-                    # resolve path
-                    include_path = os.path.realpath(os.path.dirname(self.filepath)) + '/' + include_path
-                include_config = ConfigFile(include_path)
-                include_config.load()
-                self.includes.append(include_config)
-            # loop
+            # section
+            for s in grammar_sections:
+                regex = r'^{}(?:\s+(?P<ID>.*))?\s*$'.format(s)
+                match = re.match(regex, next_line)
+                if match:
+                    section_id = match.group(1)
+                    logging.debug('new section {}'.format(section_id))
+                    next_line = self.getNextLine()
+                    content = {}
+                    while(not re.match(r'^end\s.*$', next_line)):
+                        match = re.match(r'\s+(?P<key>(?:\w|-)+)\s*(?P<value>.*)$', next_line)
+                        if match:
+                            key = match.group(1)
+                            value = match.group(2)
+                            logging.debug('found [{}] => [{}]'.format(key, value))
+                            content[key] = value
+                        # loop
+                        next_line = self.getNextLine()
+                    section = Section(s, content, section_id)
+                    self.declarations.append(section)
+            # include ? ignored for now
+            # match = re.match(r'^include\s(?P<filepath>.*)$', next_line)
+            # if match:
+            #     filepath = match.group(1)
+            #     logging.debug('new include {}'.format(filepath))
+            #     # loading include
+            #     # relative path ?
+            #     if not filepath[0] == '/':
+            #         filepath = os.path.dirname(self.filepath) + '/' + filepath
+            #     config = ConfigFile(filepath)
+            #     config.load()
+            #     self.includes.append(config)
+            #     self.includes.append(filepath)
+            #
+            #     include = Include(filepath)
+            #     self.declarations.append(include)
             next_line = self.getNextLine()
-
-    def loadModuleSet(self, module_set_name):
-        in_module = True
-        module = ModuleSet()
-        module.setName(module_set_name)
-        logging.debug('New module-set <{}>'.format(module_set_name))
-
-        while(in_module):
-            next_line = self.getNextLine()
-            # repository ?
-            match = re.match(r'^\s+repository\s+(?P<repository>.*)\s*$', next_line)
-            if match:
-                repo = match.group(1)
-                logging.debug('repository <{}>'.format(repo))
-                module.setRepository(repo)
-                continue
-            # cmake-options ?
-            match = re.match(r'^\s+cmake-options\s+(?P<cmake_opts>.*)\s*$', next_line)
-            if match:
-                cmake_opts = match.group(1)
-                logging.debug('cmake opts <{}>'.format(cmake_opts))
-                module.setCmakeOpts(cmake_opts)
-                continue
-            # use-modules ?
-            match = re.match(r'^\s+use-modules\s+(?P<use_modules>.*)\s*$', next_line)
-            if match:
-                use_modules_str = match.group(1)
-                logging.debug('use-modules <{}>'.format(use_modules_str))
-                use_modules_list = use_modules_str.split(' ')
-                module.setUseModules(use_modules_list)
-                continue
-            # branch
-            match = re.match(r'^\s+branch\s+(?P<branch>.*)\s*$', next_line)
-            if match:
-                branch = match.group(1)
-                logging.debug('branch <{}>'.format(branch))
-                module.setBranch(branch)
-                continue
-            # end module-set ?
-            match = re.match(r'^\s*end module(?:-set)?\s*$', next_line)
-            if match:
-                in_module = False
-                logging.debug(r'end module-set')
-                continue
-            # unknown line
-            logging.debug('IN module-set | unknown syntax | <{}>'.format(next_line))
-        self.elements.append(module)
-
-    def loadModule(self, module_name):
-        in_module = True
-        module = Module()
-        module.setName(module_name)
-        logging.debug('New module <{}>'.format(module_name))
-
-
-        while(in_module):
-            # repository ?
-            next_line = self.getNextLine()
-            match = re.match(r'^\s+repository\s+(?P<repository>.*)\s*$', next_line)
-            if match:
-                repo = match.group(1)
-                logging.debug('repository <{}>'.format(repo))
-                module.setRepository(repo)
-                continue
-            # cmake-options ?
-            match = re.match(r'^\s+cmake-options\s+(?P<cmake_opts>.*)\s*$', next_line)
-            if match:
-                cmake_opts = match.group(1)
-                logging.debug('cmake opts <{}>'.format(cmake_opts))
-                module.setCmakeOpts(cmake_opts)
-                continue
-            # branch
-            match = re.match(r'^\s+branch\s+(?P<branch>.*)\s*$', next_line)
-            if match:
-                branch = match.group(1)
-                logging.debug('branch <{}>'.format(branch))
-                module.setBranch(branch)
-                continue
-            # end module ?
-            match = re.match(r'^\s*end module\s*$', next_line)
-            if match:
-                in_module = False
-                logging.debug(r'end module')
-                continue
-            # unknown line
-            logging.debug('IN module | unknown syntax | <{}>'.format(next_line))
-            # loop
-            next_line = self.getNextLine()
-        self.elements.append(module)
 
     def getStandardItem(self):
         item = QStandardItem(self.filepath)
@@ -190,8 +98,11 @@ class ConfigFile():
 
 class Config():
 
-    def __init__(self, filepath):
-        self.root = ConfigFile(filepath)
+    def __init__(self, masterfile=None):
+        self.masterfile = masterfile
+        self.root = ConfigFile(self.masterfile)
+
+    def load(self):
         self.root.load()
 
     def getFileTreeModel(self):
@@ -201,3 +112,4 @@ class Config():
         root.appendRow(self.root.getStandardItem())
 
         return file_treemodel
+
--- MainWindow.py
+++ MainWindow.py
@@ -3,10 +3,117 @@ import logging
 
 
 from PyQt5.QtWidgets import QMainWindow,QDesktopWidget,QFileDialog
-from PyQt5.QtCore import Qt,QDir
+from PyQt5.QtCore import Qt,QDir,QAbstractItemModel,QVariant,QModelIndex
 
 from Config import Config
 from ui_mainwindow import Ui_MainWindow
+
+class TreeItem(object):
+
+    def __init__(self, data, parent=None):
+        self.data = data
+        self.parent = parent
+        self.children = []
+
+    def dataAt(self, row):
+        if (row >= self.columnCount()):
+            return ""
+        return self.data[row]
+
+    def addChild(self, item):
+        item.parent = self
+        self.children.append(item)
+
+    def child(self, row):
+        return self.children[row]
+
+    def childCount(self):
+        return len(self.children)
+
+    def columnCount(self):
+        return len(self.data)
+
+    def row(self):
+        if self.parent:
+            return self.parent.children.index(self)
+        return 0
+
+class TreeModel(QAbstractItemModel):
+
+    def __init__(self, header_data, parent=None):
+        super().__init__(parent)
+        self.parents = []
+        self.root = TreeItem(header_data)
+
+    def columnCount(self, parent):
+       if parent.isValid():
+           return parent.internalPointer().columnCount()
+       else:
+           return self.root.columnCount()
+
+    def rowCount(self, parent):
+       if parent.column() > 0:
+           return 0
+       if not parent.isValid():
+           parentItem = self.root
+       else:
+           parentItem = parent.internalPointer()
+       return parentItem.childCount()
+
+    def flags(self, index):
+        if not index.isValid():
+           return Qt.NoItemFlags
+        return super().flags(index)
+
+    def headerData(self, section, orientation, role=None):
+        if (orientation == Qt.Horizontal and role == Qt.DisplayRole):
+            return self.root.data[section]
+
+        return QVariant()
+
+    def index(self, row, column, parent):
+        if not self.hasIndex(row, column, parent):
+            return QModelIndex()
+
+        if parent.isValid():
+            parent_item = parent.internalPointer()
+        else:
+            parent_item = self.root
+
+        return self.createIndex(row, column, parent_item.child(row))
+
+    def parent(self, index):
+        if not index.isValid():
+            return QModelIndex()
+
+        child_item = index.internalPointer()
+        parent_item = child_item.parent
+
+        if (parent_item == self.root):
+            return QModelIndex()
+
+        return self.createIndex(parent_item.row(), 0, parent_item)
+
+    def data(self, index, role):
+        if not index.isValid():
+            return None
+        if role != Qt.DisplayRole:
+            return None
+
+        item = index.internalPointer()
+
+        return QVariant(item.dataAt(index.column()))
+
+    def setupModelData(self, config_root):
+        for d in config_root.declarations:
+            section_data = [d.type, d.id]
+            section_item = TreeItem(section_data)
+            for pair in d.content.items():
+                sub_item_data = [pair[0], pair[1]]
+                sub_item = TreeItem(pair)
+                section_item.addChild(sub_item)
+            self.root.addChild(section_item)
+
 
 class MainWindow(QMainWindow, Ui_MainWindow):
 
@@ -21,8 +128,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     def setupActions(self):
         self.actionLoadConfigFile.triggered.connect(self.handleActionLoadConfigFile)
-        # file_treeview
-        self.file_treeview.clicked.connect(self.handleFileTreeViewClicked)
+        self.actionCreateConfigFile.triggered.connect(self.handleActionCreateConfigFile)
 
     def center(self):
         screen = QDesktopWidget().screenGeometry()
@@ -38,12 +144,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         for item in checked_items:
             print(item)
 
-    def handleFileTreeViewClicked(self, index):
-        filename = index.data()
-        with open(filename, 'r') as f:
-            content = f.read()
-            self.textbrowser.setText(content)
-
     def find_checked_items(self, item):
         checked_items = []
         if (item.hasChildren()):
@@ -54,22 +154,34 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             checked_items.append(item.text())
         return checked_items
 
+    def handleActionCreateConfigFile(self):
+        filename = QFileDialog.getSaveFileName(self,
+                                       "New configuration file",
+                                       QDir.homePath(),
+                                       "*")
+         # getSavedFileName returns a tuple
+        filename = filename[0]
+        self.config = Config(filename)
+        self.displayConfig(self.config)
+
+
     def handleActionLoadConfigFile(self):
         filename = QFileDialog.getOpenFileName(self,
                                                "Select kdesrc-buildrc configuration file to load",
                                                QDir.homePath(),
-
                                                "*")
+        # getOpenFileName returns a tuple
+        filename = filename[0]
         if filename:
-            # getOpenFileName returns a tuple
-            filename = filename[0]
             self.config = Config(filename)
-            self.updateFileView()
+            self.config.load()
+            self.displayConfig(self.config)
 
-    def updateFileView(self):
-        model = self.config.getFileTreeModel()
-        self.file_treeview.setModel(model)
-
+    def displayConfig(self, config):
+        treemodel = TreeModel(["Declaration", "Value"])
+        treemodel.setupModelData(config.root)
+        self.element_treeview.setModel(treemodel)
+        self.statusbar.showMessage(config.masterfile)
 
     def resetItemCheckState(self, item):
         if (item.hasChildren()):
